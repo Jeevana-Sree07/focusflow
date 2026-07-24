78da36579601e71c33b03f508fe4d23fcb36dc67
Jeevana-Sree07 <jeevanasree995@gmail.com>
2026-07-24 17:26:18 +0530

    one

---
+++ src/test/java/com/FocusFlow/DaySync/DaySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FocusFlow.DaySync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
